1afde58a7f2bc67d16e605c5d83c932d9947f2a6
Avram Eisner <avrame@gmail.com>
2010-10-26 22:06:30 -0700

    Electrified 8ball animation added to homepage.

--- src/com/gorillagizmos/radio8ball/Radio8Ball.java
+++ src/com/gorillagizmos/radio8ball/Radio8Ball.java
@@ -1,12 +1,16 @@
 package com.gorillagizmos.radio8ball;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Radio8Ball extends Radio8BallActivity implements OnClickListener {
+	AnimationDrawable eightBallAnimation;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,15 @@ public class Radio8Ball extends Radio8BallActivity implements OnClickListener {
         
         Button askButton = (Button) findViewById(R.id.ask_button);
         askButton.setOnClickListener(this);
+        
+        ImageView eightBallImage = (ImageView) findViewById(R.id.eight_ball_image);
+        eightBallImage.setBackgroundResource(R.anim.eight_ball);
+        eightBallAnimation = (AnimationDrawable) eightBallImage.getBackground();
+    }
+    
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+    	eightBallAnimation.start();
     }
 
 	@Override
